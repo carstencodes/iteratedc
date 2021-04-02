0cec04a69679c4c4915471e6880742bfdefb3256
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2021-04-02 15:03:18 +0200

    Changed repository settings for CI/CD

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 #
 # Copyright (c) 2021 Carsten Igel.
 #
-# This file is part of nt2dc
-# (see https://github.com/carstencodes/nt2dc).
+# This file is part of iteratedc
+# (see https://github.com/carstencodes/iteratedc).
 #
 # License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
 #
