3678396719eff3a701e9140b34ea33a305954e15
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2021-04-03 13:18:13 +0200

    Added documentation comments.

--- src/iteratedc/tree.py
+++ src/iteratedc/tree.py
@@ -7,6 +7,9 @@
 # License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
 #
 
+"""Implementation of a raw tree structure for dataclasses including the nodes of the tree.
+"""
+
 from dataclasses import is_dataclass, fields, Field, asdict
 from typing import (
     Iterable,
@@ -73,6 +76,8 @@ def _iterate_over_current_item(
 
 
 class Node:
+    """Implementation of a tree node for dataclass members.
+    """
     def __init__(
         self,
         value: _FieldValueWithMetaData,
@@ -83,14 +88,29 @@ class Node:
 
     @property
     def item(self) -> Any:
+        """The field instance.
+
+        Returns:
+            Any: The current instance of the field.
+        """
         return self.__field.value
 
     @property
     def item_index(self) -> Optional[int]:
+        """The index in a parent collection represented by the field.
+
+        Returns:
+            Optional[int]: The index or None, if the field is no collection.
+        """
         return self.__field.field_index
 
     @property
     def meta_data(self) -> Mapping[str, Any]:
+        """Gets the meta data of the field.
+
+        Returns:
+            Mapping[str, Any]: A mapping of field meta data values.
+        """
         fld: Optional[Field] = self.__field.corresponding_field
         if fld is None:
             return {}
@@ -99,6 +119,11 @@ class Node:
 
     @property
     def name(self) -> str:
+        """The name of the field.
+
+        Returns:
+            str: The name of the field. Might be empty.
+        """
         fld: Optional[Field] = self.__field.corresponding_field
         if fld is None:
             return ""
@@ -107,23 +132,40 @@ class Node:
 
     @property
     def type(self) -> Type:
+        """The type of the current value.
+
+        Returns:
+            Type: The type of the current value.
+        """
         fld: Optional[Field] = self.__field.corresponding_field
-        if fld is None:
+        if fld is None or self.item_index is not None:
             return type(self.item)
 
         return fld.type
 
     @property
     def children(self) -> Iterable["Node"]:
+        """The child nodes.
+
+        Returns:
+            [type]: The nodes following the current node.
+        """
         return self.__children
 
 
 class Tree:
+    """Implements a tree as a sequence of nodes.
+    """
     def __init__(self, root: Node) -> None:
         self.__root = root
 
     @property
     def root(self) -> Node:
+        """The root node of the tree.
+
+        Returns:
+            Node: The root node.
+        """
         return self.__root
 
 
@@ -147,6 +189,15 @@ def _create_node_from_item(field_with_meta: _FieldValueWithMetaData) -> Node:
 
 
 def create_tree_from_dataclass(data_class: Any) -> Optional[Tree]:
+    """Takes the specified dataclass and transforms it to a tree.
+
+    Args:
+        data_class (Any): The data class to analyze.
+
+    Returns:
+        Optional[Tree]: The tree. If the argument is None
+                        or no dataclass, the result will be None.
+    """
     if data_class is None or not is_dataclass(data_class):
         return None
 
