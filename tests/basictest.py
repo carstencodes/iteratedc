2130e081811d79956e50a1a0e65d2926a05ac6d4
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2021-04-03 13:31:48 +0200

    Added a small test case

---
+++ tests/basictest.py
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2021 Carsten Igel.
+#
+# This file is part of iteratedata_class
+# (see https://github.com/carstencodes/iteratedata_class).
+#
+# License: 3-clause BSD, see https://opensource.org/licenses/BSD-3-Clause
+#
+
+import iteratedc
+import unittest
+
+
+class BasicTest(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
